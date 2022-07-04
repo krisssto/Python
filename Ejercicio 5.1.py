1de88bb4753395ccff54bc7aae3755a967794222
krisssto <krisssto@gmail.com>
2022-07-04 00:49:50 -0300

    Add files via upload

---
+++ Ejercicio 5.1.py
@@ -0,0 +1,8 @@
+altura = float(input('Cual es la altura del triangulo? '))
+base = float(input('Cual es la base del triangulo? '))
+superficie = ((altura*base)/2)
+print (superficie)
+
+radio = float(input("Cual es el radio del circulo? "))
+area = (radio*radio)*3.14
+print (area)
